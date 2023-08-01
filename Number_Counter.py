c41041fb0e7b37203d4eed005a46766121c46581
Nick Reskos <140502271+nikosresk1997@users.noreply.github.com>
2023-08-01 13:41:35 +0300

    Delete Number_Counter.py

--- Number_Counter.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def number_counter(string):
-    numbers=re.findall("\d+",string)
-    return len(numbers)
-
-string=input("Give me any string and I will count how many numbers it contains\n")
-num_count=number_counter(string)
-print(num_count)
